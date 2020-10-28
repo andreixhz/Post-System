ccae29e57c230dee5afcf23be3b3d77a3a68deb4
andrei <andreixhz123@gmail.com>
2020-10-28 13:25:55 -0300

    Home with valid login

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React , { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+// import { Container } from './styles';
+
+
+
+function Home() {
+    const history = useHistory();
+    useEffect(() => {
+        if(!localStorage.getItem("token")){
+            history.push('/login');
+        } 
+    }, [])  
+
+    return <div />;
+}
+
+export default Home;
