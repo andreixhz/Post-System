09effd9f0f24590fd2c054a1d00d6faecccac81d
andrei <andreixhz123@gmail.com>
2020-10-28 14:01:01 -0300

    cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 require('./app/controllers/index')(app);
 
