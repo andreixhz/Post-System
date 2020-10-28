df1d8effb334e8782e6fe71f75602bbe211fb98d
andrei <andreixhz123@gmail.com>
2020-10-28 13:31:52 -0300

    home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,8 +2,6 @@ import React , { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 // import { Container } from './styles';
 
-
-
 function Home() {
     const history = useHistory();
     useEffect(() => {
