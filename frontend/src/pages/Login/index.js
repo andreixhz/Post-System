9b608d47519e1a38fa8ebd073c89147a95b6688b
andrei <andreixhz123@gmail.com>
2020-10-28 12:58:28 -0300

    Login screen

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+function Login() {
+    return(
+        <div className="center-a enter">
+            <div className="enter-form">
+                <h2>Login</h2>
+                <form>
+                    <TextField
+                        label="Username"
+                        id="outlined-size-small"
+                        defaultValue=""
+                        variant="outlined"
+                        size="small"
+                    />
+                    <TextField
+                        label="Password"
+                        id="outlined-size-small"
+                        defaultValue=""
+                        type="password"
+                        variant="outlined"
+                        size="small"
+                    />
+                    <div>
+                        <Button variant="outlined">Confirm</Button>
+                        <Link to="/register">Don't have account?</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
