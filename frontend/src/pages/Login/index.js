425b420424aa0b440263f235fec63a9dfd9effb5
andrei <andreixhz123@gmail.com>
2020-10-28 14:19:19 -0300

    function from login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import Api from '../../api';
+import { useState } from 'react';
 
 function Login() {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = event => {
+
+        Api({
+            method: 'post',
+            url:'/auth/authenticate',
+            data:{
+                username, password
+        }})
+        .then(res => {
+            localStorage.setItem("token", res.data.token);
+        })
+        .catch(() => alert("user not found"));
+     
+        event.preventDefault();
+      };
+    
+
     return(
         <div className="center-a enter">
             <div className="enter-form">
                 <h2>Login</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <TextField
+                        required
+                        onChange={(e) => setUsername(e.target.value)}
                         label="Username"
                         id="outlined-size-small"
                         defaultValue=""
@@ -16,6 +41,8 @@ function Login() {
                         size="small"
                     />
                     <TextField
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
                         label="Password"
                         id="outlined-size-small"
                         defaultValue=""
@@ -24,8 +51,8 @@ function Login() {
                         size="small"
                     />
                     <div>
-                        <Button variant="outlined">Confirm</Button>
-                        <Link type="submit" to="/register">Don't have account?</Link>
+                        <Button type="submit" variant="outlined">Confirm</Button>
+                        <Link to="/register">Don't have account?</Link>
                     </div>
                 </form>
             </div>
