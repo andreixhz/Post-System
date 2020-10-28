c27d275338da11f2cb58ca4169766d5bf6f5948e
andrei <andreixhz123@gmail.com>
2020-10-28 12:06:51 -0300

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Routes from './routes';
+
+import './assets/styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Routes/>
   );
 }
 
---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+function Routes() {
+    return (
+        <BrowserRouter className>
+            <Menu/>
+            <div className="center">
+                <div className="container">
+                    
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
