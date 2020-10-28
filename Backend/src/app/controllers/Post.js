edc6ed713cf87ae12cbbd9058a5b74f23a5f70d4
andrei <andreixhz123@gmail.com>
2020-10-28 11:32:13 -0300

    upload image finished

--- Backend/src/app/controllers/Post.js
+++ Backend/src/app/controllers/Post.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+const uploadMiddleware = require('../middlewares/upload');
+const authMiddleware = require('../middlewares/auth')
 
-router.post('/', async (req, res) => {
- 
-    
+router.use(authMiddleware);
+
+router.post('/', uploadMiddleware.single('image'), async (req, res) => {
+
+    const { filename, size } = req.file;
+
+    const post = await Post.create({uuid: req.userId, image: filename, description: req.description});
+
+    return res.status(200).json({ image: `/uploads/${filename}`, size, post})
+
 });
+
 
 
 module.exports = app => app.use('/post', router);
