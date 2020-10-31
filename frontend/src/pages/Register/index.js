4c1f6850db1cc57901e714cda3d78f62b1e112b3
andrei <andreixhz123@gmail.com>
2020-10-31 04:52:01 -0300

    register screen

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import Api from '../../api';
+import {useState} from 'react';
 
 function Register() {
+
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const history = useHistory();
+
+    const handleSubmit = event => {
+
+        Api({
+            method: 'post',
+            url:'/auth/register',
+            data:{
+                username, email, password
+            }
+        })
+        .then(res => {
+            localStorage.setItem("token", res.data.token);
+            history.push('/');
+        })
+        .catch(() => alert("register error"));
+     
+        event.preventDefault();
+      };
+    
+
     return(
         <div className="center-a enter">
             <div className="enter-form">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <TextField
                         label="Username"
                         id="outlined-size-small"
                         defaultValue=""
                         variant="outlined"
                         size="small"
+                        onChange={(e) => setUsername(e.target.value)}
                     />
                     <TextField
                         label="Email"
@@ -22,6 +51,7 @@ function Register() {
                         type="email"
                         variant="outlined"
                         size="small"
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     <TextField
                         label="Password"
@@ -30,6 +60,7 @@ function Register() {
                         type="password"
                         variant="outlined"
                         size="small"
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <div>
                         <Button type="submit" variant="outlined">Confirm</Button>
